77b478210a863fa1492e63d4b5709705d726e9bb
MidhaTahir <midhatahirkhan2011@gmail.com>
2021-07-01 14:09:35 +0500

    initial commit and meta tags query hook

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Intro to gatsby',
+    description: 'A site I built following fem amazing gatsby workshop',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+};
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout';
 
 export default () => (
   <Layout>
-    <h1>Home</h1>
-    <p>Hello World</p>
-    <Link to="/about">&rarr;About Page</Link>
+    <h1>About Me</h1>
+    <p>This is my personal blog website</p>
+    <Link to="/">&larr; back to home</Link>
   </Layout>
 );
